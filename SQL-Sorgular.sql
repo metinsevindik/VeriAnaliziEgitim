0e263218c030845b44781c5a4091ce807470f200
METİN SEVİNDİK <sevindikmetin@gmail.com>
2020-02-15 16:53:08 +0300

    Create SQL-Sorgular.sql

---
+++ SQL-Sorgular.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Kisi (
+    KisiID int NOT NULL PRIMARY KEY,
+    Ad varchar(255),
+    Soyad varchar(255),
+    Adres varchar(255),
+    Sehir varchar(255)
+);
+
+insert into Kisi (Ad,Soyad,Adres,Sehir) values ('Metin','Sevindik','ORDU','ANKARA')
+
